09be5f4fcd6fab6a055ef598f63b5799bb507202
Andrew Dippel <a.t.dippel@gmail.com>
2022-08-22 17:29:30 +1000

    Add timer

--- Assets/jsvascript.js
+++ Assets/jsvascript.js
@@ -1,4 +1,24 @@
-var startquiz = document.querySelector(".startquiz");
-var highscorepage = document.querySelector(".highscore");
-var backtogame = document.querySelector(".backtogame");
-var submit = document.querySelector(".submit");
+var startButton = document.getElementById("startQuiz");
+var highscorePage = document.getElementById("highscore");
+var backToGame = document.getElementById("backToGame");
+var submit = document.getElementById("submit");
+var timerElement = document.getElementById("timerElement");
+var timerCount = 75;
+
+function startQuiz() {
+    console.log("yes")
+
+var timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = "Timer: " + timerCount;
+    if (timerCount >= 0) {
+        
+    }
+        if (timerCount === 0) {
+        clearInterval(timer);
+        }
+
+}, 1000);
+}
+
+startButton.addEventListener("click", startQuiz);
