3ad452649ecb798c257d6dec72033d7b888c96c6
Andrew Dippel <a.t.dippel@gmail.com>
2022-08-22 16:24:36 +1000

    add hover to buttons and and sections for questions and finish screen

--- Assets/jsvascript.js
+++ Assets/jsvascript.js
@@ -1,1 +1,4 @@
 var startquiz = document.querySelector(".startquiz");
+var highscorepage = document.querySelector(".highscore");
+var backtogame = document.querySelector(".backtogame");
+var submit = document.querySelector(".submit");
