c9314d66c4f28a364135f250209cfe13c981be2a
Andrew Dippel <a.t.dippel@gmail.com>
2022-08-24 16:52:38 +1000

    push questions to question section

--- Assets/jsvascript.js
+++ Assets/jsvascript.js
@@ -3,24 +3,30 @@ var highscorePage = document.getElementById("highscore");
 var backToGame = document.getElementById("backToGame");
 var submit = document.getElementById("submit");
 var timerElement = document.getElementById("timerElement");
-var timerCount = 3;
+var displayQuestion = document.getElementsByClassName(".question");
+var timerCount = 75;
 var win = false;
 var score = 0;
 var questions = [{
-    question: "Commonly used data types DO NOT include:"\n(a) 'String'\n(b) 'Boolean'\n(c) 'Alerts'\n(d) 'Numbers',
-    answer: "c"
+    question: "Commonly used data types DO NOT include:",
+    choices: ['a. String', '.b Boolean', '.c Alerts', '.d Numbers'],
+    answer: '.c Alerts'
 }, {
-    question: "The condition in an if / else statement is enclosed within _____."\n(a) 'Quotes'\n(b) 'Curly Brackets'\n(c) 'Parenthesis'\n(d) 'Square Brackets',
-    answer: "b"
+    question: "The condition in an if / else statement is enclosed within _____.",
+    choices: ['.a Quotes', '.b Curly Brackets', '.c Parenthesis', '.d Square Brackets'],
+    answer: '.b Curly Brackets'
 }, {
-    question: "Arrays in JavaScript can be used to store _____."\n(a) 'Numbers and Strings'\n(b) 'Other Arrays'\n(c) 'Booleans'\n(d) 'All Of The Above',
-    answer: "d"
+    question: "Arrays in JavaScript can be used to store _____.",
+    choices: ['.a Numbers and Strings', '.b Other Arrays', '.c Booleans', '.d All Of The Above'],
+    answer: '.d All Of The Above'
 }, {
-    question: "String values must be enclosed within _____ when being assigned to variables."\n(a) 'Commas'\n(b) 'Curly Brackets'\n(c) 'Quotes'\n(d) 'Parenthesis',
-    answer: "a"
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ['.a Commas', '.b Curly Brackets', '.c Quotes', '.d Parenthesis'],
+    answer: '.a Commas'
 }, {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:" \n(a) 'JavaScript'\n(b) 'Terminal/Bash'\n(c) 'For Loops'\n(d) 'console.log',
-    answer: "d"
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ['.a JavaScript', '.b Terminal/Bash', '.c For Loops', '.d console.log'],
+    answer: '.d console.log'
 }];
 
 function startQuiz() {
@@ -39,14 +45,23 @@ function startQuiz() {
         }
     }, 1000);
 }
-for (var i = 0; i < questions.length; i++) {
+function generateQuestion() {
+    for (var i = 0; i < questions.length; i++) {
+        startButton.addEventListener("click", pushQuestion() {
+            displayQuestion.push("questions[0]")
+        })
+    } console.log(i);
+}
+
+
+/*for (var i = 0; i < questions.length; i++) {
     var response = document.
         if(response == question[i].answer) {
             score++;
 } else {
     timerCount - 10;
 }
-}
+}*/
 
 
 startButton.addEventListener("click", startQuiz);
