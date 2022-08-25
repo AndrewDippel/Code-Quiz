37405df81a03d42c091eb6a45e3a8ec5cf1e5e65
Andrew Dippel <a.t.dippel@gmail.com>
2022-08-25 16:16:52 +1000

    trying to get question to appear

--- Assets/jsvascript.js
+++ Assets/jsvascript.js
@@ -4,6 +4,9 @@ var backToGame = document.getElementById("backToGame");
 var submit = document.getElementById("submit");
 var timerElement = document.getElementById("timerElement");
 var displayQuestion = document.getElementsByClassName(".question");
+var input = document.getElementById("initialsInput");
+var printInput = localStorage.getItem("storeInput");
+var submit = document.getElementById("submit");
 var timerCount = 75;
 var win = false;
 var score = 0;
@@ -29,6 +32,7 @@ var questions = [{
     answer: '.d console.log'
 }];
 
+//timer that consol.logs if you win or loose
 function startQuiz() {
     console.log("yes")
 
@@ -45,14 +49,21 @@ function startQuiz() {
         }
     }, 1000);
 }
+//generate question to html using dom manipulation
 function generateQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
     var createQuestion = document.createElement("h2");
     var questionText = document.createTextNode("questions[0]");
     createQuestion.appendChild(questionText);
     Document.displayQuestion.appendChild(createQuestion);
 }
+//stor input from form to be displayed on highscore page
+function storeInput() {
+    localStorage.setItem("input", JSON.stringify(input));
+    console.log("printInput: ", JSON.parse(printInput));
+}
 
-
+//for loop to check if answer is correct and to add to score or if loose minus 10 seconds from time  
 /*for (var i = 0; i < questions.length; i++) {
     var response = document.
         if(response == question[i].answer) {
